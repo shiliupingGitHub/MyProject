4c8568469b190cdf6dbf0386f1245238fc9b1156
shiliupingGitHub <582441842@qq.com>
2024-02-23 17:20:45 +0800

    基础移动

--- Assets/Game/Script/Character/Controller/GamePlayerController.cs
+++ Assets/Game/Script/Character/Controller/GamePlayerController.cs
@@ -12,28 +12,100 @@ namespace Game.Script.Character
         public InputActionReference MoveDownAction;
         public InputActionReference MoveLeftAction;
         public InputActionReference MoveRightAction;
+        public float MoveSpeed = 1;
+        
+        private Vector3 moveDir = Vector3.zero;
+        private bool bInitCamera = false;
+        private bool bCheckCamera = false;
+        
         private void Update()
         {
-            if (isOwned)
+            if (isLocalPlayer)
             {
-                
+               DoMove();
+               SetUpCamera();
             }
         }
 
-        public override void OnStartAuthority()
+        void DoMove()
         {
-            base.OnStartAuthority();
+            var dir = moveDir;
+            dir.Normalize();
+
+            transform.position +=  dir * MoveSpeed * Time.deltaTime;
+        }
+
+        void SetUpInput()
+        {
             MoveUpAction.action.Enable();
+            MoveDownAction.action.Enable();
+            MoveLeftAction.action.Enable();
+            MoveRightAction.action.Enable();
             
             MoveUpAction.action.started += context =>
             {
-                
+                moveDir.y += 1;
             };
             MoveUpAction.action.canceled += context =>
             {
-               
+                moveDir.y -= 1;
             };
-
+            
+            MoveDownAction.action.started += context =>
+            {
+                moveDir.y -= 1;
+            };
+            MoveDownAction.action.canceled += context =>
+            {
+                moveDir.y += 1;
+            };
+            
+            MoveLeftAction.action.started += context =>
+            {
+                moveDir.x -= 1;
+            };
+            MoveLeftAction.action.canceled += context =>
+            {
+                moveDir.x += 1;
+            };
+            
+            MoveRightAction.action.started += context =>
+            {
+                moveDir.x += 1;
+            };
+            MoveRightAction.action.canceled += context =>
+            {
+                moveDir.x -= 1;
+            };
         }
+
+        void SetUpCamera()
+        {
+            if (!bInitCamera)
+            {
+                if (bCheckCamera)
+                {
+                    var mainCamera = Camera.main;
+
+                    if (mainCamera)
+                    {
+                        mainCamera.transform.SetParent(transform);
+
+                        mainCamera.transform.localPosition = Vector3.zero;
+                        mainCamera.transform.localEulerAngles = Vector3.zero;
+                        bInitCamera = true;
+                    }
+                }
+            }
+            
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+            SetUpInput();
+            bCheckCamera = true;
+        }
+        
     }
 }
--- Assets/Game/Script/Misc/GameNetworkManager.cs
+++ Assets/Game/Script/Misc/GameNetworkManager.cs
@@ -1,10 +1,22 @@
 ﻿using Mirror;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace Game.Script.Misc
 {
     public class GameNetworkManager : NetworkManager
     {
-        
+        public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+        {
+            Transform startPos = GetStartPosition();
+            GameObject player = startPos != null
+                ? Instantiate(playerPrefab, startPos.position, startPos.rotation)
+                : Instantiate(playerPrefab, Vector3.zero, quaternion.identity);
+
+            // instantiating a "Player" prefab gives it the name "Player(clone)"
+            // => appending the connectionId is WAY more useful for debugging!
+            player.name = $"{playerPrefab.name} [connId={conn.connectionId}]";
+            NetworkServer.AddPlayerForConnection(conn, player);
+        }
     }
 }
