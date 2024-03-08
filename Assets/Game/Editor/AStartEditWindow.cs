5a5d74351827a66f2f02b95743c3918e52435fe4
shiliupingGitHub <582441842@qq.com>
2024-03-08 17:05:34 +0800

    Astar

---
+++ Assets/Game/Editor/AStartEditWindow.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace Game.Editor
+{
+    public class AStartEditWindow : EditorWindow
+    {
+        private bool bEnableEdit = false;
+        [MenuItem("AStar/Edit")]
+        private static void ShowWindow()
+        {
+            var window = GetWindow<AStartEditWindow>();
+            window.titleContent = new GUIContent("AStar");
+            window.Show();
+        }
+      
+        private void OnGUI()
+        {
+            bEnableEdit = GUILayout.Toggle(bEnableEdit, "启用编辑");
+        }
+
+        private void OnEnable()
+        {
+            SceneView.duringSceneGui += OnSceneView;
+        }
+
+        private void OnDestroy()
+        {
+            SceneView.duringSceneGui -= OnSceneView;
+        }
+
+        void OnSceneView(SceneView sceneView)
+        {
+            if (bEnableEdit)
+            {
+                HandleUtility.AddDefaultControl(GUIUtility.GetControlID(FocusType.Passive));
+                
+               
+            }
+        }
+    }
+}
