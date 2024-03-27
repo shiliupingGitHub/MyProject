741eb6a6d6819042eb00ddc401fd372e1501eeb3
shiliupingGitHub <582441842@qq.com>
2024-03-27 11:34:05 +0800

    12

--- Assets/Game/Script/UI/Frames/FightFrame.cs
+++ Assets/Game/Script/UI/Frames/FightFrame.cs
@@ -21,26 +21,32 @@ namespace Game.Script.UI.Frames
             {
                 // var pathSystem = GameInstance.Instance.GetSubsystem<PathSubsystem>();
                 //
-                // var points = pathSystem.DoPath(new Vector3(4.55f, 0.53f, 0), GameInstance.Instance.MyController.transform.position, GameInstance.Instance.MapScript);
+                // var points = pathSystem.DoPath(new Vector3(4.55f, 0.53f, 0), GameInstance.Instance.MyController.transform.position + new Vector3(0.5f, 0.5f, 0), GameInstance.Instance.MapScript);
                 //
                 // if (_lineRenderer == null)
                 // {
                 //     GameObject go = new GameObject("testpath");
                 //     _lineRenderer = go.AddComponent<LineRenderer>();
-                //     
+                //     _lineRenderer.startWidth = 0.1f;
+                //     _lineRenderer.endWidth = 0.1f;
+                //
                 // }
                 //
                 // List<Vector3> linePoints = new();
-                // _lineRenderer.positionCount = points.Count;
-                // foreach (var point in points)
+                // _lineRenderer.positionCount = points?.Count??0;
+                // if (points != null)
                 // {
-                //     var o = GameInstance.Instance.MapScript.transform.position;
-                //     var grid = GameInstance.Instance.MapScript.MyGrid;
-                //     var p = new Vector3(point.Item1 * grid.cellSize.x  +  grid.cellSize.x * 0.5f, point.Item2 * grid.cellSize.y  + grid.cellSize.y * 0.5f, -1);
-                //     p += o;
-                //     linePoints.Add(p);
+                //     foreach (var point in points)
+                //     {
+                //         var o = GameInstance.Instance.MapScript.transform.position;
+                //         var grid = GameInstance.Instance.MapScript.MyGrid;
+                //         var p = new Vector3(point.Item1 * grid.cellSize.x  +  grid.cellSize.x * 0.5f, point.Item2 * grid.cellSize.y  + grid.cellSize.y * 0.5f, -1);
+                //         p += o;
+                //         linePoints.Add(p);
+                //     }
+                //     _lineRenderer.SetPositions(linePoints.ToArray());
                 // }
-                // _lineRenderer.SetPositions(linePoints.ToArray());
+             
             });
         }
     }
