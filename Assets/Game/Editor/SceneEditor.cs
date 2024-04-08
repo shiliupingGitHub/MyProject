cd324be9a99c02838adc73781bf62c9a7e474e55
shiliupingGitHub <582441842@qq.com>
2024-04-08 15:49:29 +0800

    1212

--- Assets/Game/Editor/SceneEditor.cs
+++ Assets/Game/Editor/SceneEditor.cs
@@ -19,5 +19,21 @@ namespace Game.Editor
             PrefabUtility.UnpackPrefabInstance(go, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
             GameSetting.Instance.ShowGrid = true;
         }
+
+        [MenuItem("GameObject/复制Hierarchy路径")]
+        static void CopyHierarchyPath()
+        {
+            var gameObject = Selection.activeGameObject;
+            string path = gameObject.name;
+            Transform t = gameObject.transform;
+            while (t.parent != null)
+            {
+                t = t.parent;
+                path = t.name + "/" + path;
+            }
+ 
+            // 将路径复制到剪贴板
+            GUIUtility.systemCopyBuffer = path;
+        }
     }
 }
