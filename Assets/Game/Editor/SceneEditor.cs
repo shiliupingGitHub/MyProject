66a5b75d5c79eba705f527b2297a7e42851fde5d
shiliupingGitHub <582441842@qq.com>
2024-04-08 13:21:21 +0800

    121

---
+++ Assets/Game/Editor/SceneEditor.cs
@@ -0,0 +1,23 @@
+﻿
+
+using Game.Script.Res;
+using Game.Script.Setting;
+using UnityEditor;
+using UnityEngine;
+
+namespace Game.Editor
+{
+    
+    public class SceneEditor : UnityEditor.Editor
+    {
+        [MenuItem("GameObject/CreateMapBk")]
+        static void CreateMapBk()
+        {
+            var template = GameResMgr.Instance.LoadAssetSync<GameObject>("Assets/Game/Res/Map/Bk/SceneTemplate.prefab");
+            var go = PrefabUtility.InstantiatePrefab(template) as GameObject;
+            go.name = "newMapBk";
+            PrefabUtility.UnpackPrefabInstance(go, PrefabUnpackMode.Completely, InteractionMode.AutomatedAction);
+            GameSetting.Instance.ShowGrid = true;
+        }
+    }
+}
